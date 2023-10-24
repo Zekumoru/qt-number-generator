16d9c7324f117ecd1093f37e1292e55c6a545440
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-10-24 19:17:12 +0200

    Add min widget values init

--- widgets/maxwidget.cpp
+++ widgets/maxwidget.cpp
@@ -1,9 +1,13 @@
 #include "maxwidget.h"
 #include "datawidget.h"
+#include <climits>
 
 MaxWidget::MaxWidget(QWidget *parent)
     : DataWidget{tr("Max"), parent}
 {
+    allValue = INT_MIN;
+    evenValue = INT_MIN;
+    oddValue = INT_MIN;
 }
 
 void MaxWidget::addValue(int value)
